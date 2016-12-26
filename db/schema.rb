5ddd3fabd1838a67393cff57c75f7a7dc3d0ec7d
Victor Macias <v.maciascastillo@gmail.com>
2016-12-26 17:38:34 +0100

    layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161226104043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.string   "involvement"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.destroy_all
+
+decoswap = Project.new
+decoswap.name = "Decoswap"
+decoswap.description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, aut, fuga? Temporibus veniam ipsum labore tempora consequatur culpa necessitatibus officia accusamus, similique, consectetur ratione impedit ex reiciendis fuga esse maiores."
+decoswap.url = "www.decoswap.co"
+decoswap.involvement = "Lead backend developper"
+decoswap.save
