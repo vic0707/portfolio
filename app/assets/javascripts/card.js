e066c21d1fafdbb5d3569ec1daaeb25bd4060a82
Victor Macias <v.maciascastillo@gmail.com>
2016-12-28 11:41:39 +0100

    photos and social icons

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
-	var image = $('#decoswap').css('background-image');
+	var decoswap = $('#decoswap').css('background-image');
+	var construction = $('#under-construction').css('background-image');
 	$('#decoswap').hover(
 		function() {
 			$( this ).css("background-image", "");
-			$('.project-text').show();
+			$('#decoswap-text').show();
 		}, function() {
-			$( this ).css("background-image", image);
-			$('.project-text').hide();
-		});
+			$( this ).css("background-image", decoswap);
+			$('#decoswap-text').hide();
+	});
+	$('#under-construction').hover(
+		function() {
+			$( this ).css("background-image", "");
+			$('#construction-text').show();
+		}, function() {
+			$( this ).css("background-image", construction);
+			$('#construction-text').hide();
+	});
+	
 });
